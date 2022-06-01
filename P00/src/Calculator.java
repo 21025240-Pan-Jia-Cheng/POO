cb3f9e57991b05a8ba7b73816617bb15bb1bea5f
21025240-Pan-Jia-Cheng <106227436+21025240-Pan-Jia-Cheng@users.noreply.github.com>
2022-06-01 20:02:32 +0800

    Update Calculator.java

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -6,14 +6,22 @@ public class Calculator {
 		}
 	
 		
-		  public int subtract(int a, int b) { return a - b; }
+		  public int subtract(int a, int b) {
+			  return a - b;
+		  }
 		  
-		  public int multiply(int a, int b) { return a * b; }
+		  public int multiply(int a, int b) {
+			  return a * b;
+		  }
 		  
-		  public int divide(int a, int b) { if (b !=0) { return a / b; }
+		  public int divide(int a, int b) {
+			  if (b !=0) { 
+				  return a / b;
+			  }
 		  
 		  else { throw new IllegalArgumentException("Division by zero is not allowed");
-		  } }
+		  } 
+		  }
 		 
 	
 	
